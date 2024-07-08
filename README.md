959d7e99fd5443f0b70dc1ccc25ff9afddc54f3d
shine4335 <muichirotokito70@gmail.com>
2024-07-08 12:17:18 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-fisikaa
+# Calculator-Archimedes-Principle
--- script density.js
+++ script density.js
@@ -3,17 +3,17 @@ document.getElementById('archimedesForm').addEventListener('submit', function(ev
     
     // Ambil nilai input dari form
     const mass = parseFloat(document.getElementById('mass').value);
-    const density = parseFloat(document.getElementById('density').value);
+    const volume = parseFloat(document.getElementById('volume').value);
     
     // Validasi input
-    if (isNaN(mass) || isNaN(density)) {
+    if (isNaN(mass) || isNaN(volume)) {
         document.getElementById('result').innerText = "Silakan masukkan nilai yang valid.";
         return;
     }
     
-    // Hitung volume
-    const volume = mass / density;
+    // Hitung massa jenis
+    const density = mass / volume;
     
     // Tampilkan hasil
-    document.getElementById('result').innerText = `Volume Objek: ${volume.toFixed(2)} m³`;
+    document.getElementById('result').innerText = `Massa Jenis Objek: ${density.toFixed(2)} kg/m³`;
 });
